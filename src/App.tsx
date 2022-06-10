9457cc5539e646af06eff5e9c85007a9048f6afa
momoka <moowfaw@gmail.com>
2022-06-11 01:13:42 +0900

    💡add hint

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,67 @@ import {Box, Button, ChakraProvider, Container, Flex, FormControl, Grid, Input, 
 import {uid} from "uid";
 
 export const App = () => {
+  /**
+   * - nameInput = 入力したテキスト
+   */
   const [nameInput, setNameInput] = useState('')
+
+  /**
+   * - todos = 完了・未完了関係無くすべての要素が入る配列
+   * - name = 入力したテキスト
+   * - isDone = 完了したかどうか
+   * - uid = uid
+   */
   const [todos, setTodos] = useState<{ name: string, isDone: boolean, uid: string }[]>([])
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNameInput(e.target.value)
   }
 
+  /**
+   * TODOを追加する関数
+   * - prevState = 現在のtodos
+   * - prevStateを展開し、入力されたTODOを追加した新しい配列を生成
+   * - name = 入力したテキスト
+   * - isDone = 完了したかどうかの初期(未完了)
+   * - uid = uidを生成する関数
+   * - nameInputを初期化
+   */
   const onClickAdd = () => {
     setTodos(prevState => [...prevState, {name: nameInput, isDone: false, uid: uid()}])
     setNameInput('')
   }
 
+  /**
+   * TODOを削除する関数
+   * - 引数で受け取ったuidを持つTODOを削除
+   * - prevStateをfilterにかける
+   * - ループしている要素のuidと引数のuidを比較し、一致しない要素を抽出(TODO削除後の配列を作るため)
+   * - 抽出した要素で新しい配列を生成
+   * ※ 引数のuidとオブジェクトのプロパティのuidが重複するため、_を付けてリネーム
+   */
   const onClickRemove = (uid: string) => {
     setTodos(prevState => prevState.filter(({uid: _uid}) => uid !== _uid))
   }
 
+  /**
+   * 引数で受け取ったuidを持つTODOを完了する関数
+   * - prevState(すべての要素)をmapで回す
+   * - プロパティを返す
+   * - ループしている要素のuidと引数のuidが一致した場合、isDoneをtrueに上書き(TODOを完了させる)
+   */
   const onCLickComplete = (uid: string) => {
     setTodos(prevState => prevState.map(({name, isDone, uid: _uid}) => {
       return {name, isDone: _uid === uid ? true : isDone, uid: _uid}
     }))
   }
 
+  /**
+   * 引数で受け取ったuidを持つTODOを戻す関数
+   * - prevState(すべての要素)をmapで回す
+   * - プロパティを返す
+   * - ループしている要素のuidと引数で受け取ったuidが一致した場合、isDoneをfalseに上書き(TODOを戻す)
+   */
   const onCLickBack = (uid: string) => {
     setTodos(prevState => prevState.map(({name, isDone, uid: _uid}) => {
       return {name, isDone: _uid === uid ? false : isDone, uid: _uid}
@@ -45,6 +84,7 @@ export const App = () => {
           <Box bgColor={"blue.50"} p={10}>
             <Text fontWeight={"bold"}>未完了のTODO</Text>
             <Grid gap={5} mt={5}>
+              {/* isDoneがfalseの要素をfilterで抽出 */}
               {todos.filter(({isDone}) => !isDone).map(({name, isDone, uid}) => (
                 <Flex key={`incomplete_${uid}`} borderBottomColor={'gray.200'} borderBottomWidth={1} pb={5}
                       justifyContent={"space-between"} alignItems={"center"}>
@@ -60,6 +100,7 @@ export const App = () => {
           <Box bgColor={"green.50"} p={10}>
             <Text fontWeight={"bold"}>完了のTODO</Text>
             <Grid gap={5} mt={5}>
+              {/* isDoneがtrueの要素をfilterで抽出 */}
               {todos.filter(({isDone}) => isDone).map(({name, uid}) => (
                 <Flex key={`complete_${uid}`} borderBottomColor={'gray.200'} borderBottomWidth={1} pb={5}
                       justifyContent={"space-between"} alignItems={"center"}>
